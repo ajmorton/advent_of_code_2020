e89f05843869796a4fded49cf8fb324cfb8edf62
ajmorton <ajmorton2@gmail.com>
2020-12-04 19:21:49 +1100

    Day 4 - opt

--- day_4.py
+++ day_4.py
@@ -1,81 +1,34 @@
 import re
 
-def valid(fields):
-    to_find = ["byr", "iyr", "eyr", "hgt", 'hcl', "ecl", "pid", "cid"]
-    for key in fields.keys():
-        to_find.remove(key)
+def has_valid_fields(p: dict) -> bool:
+    try:
+        val_byr = len(p["byr"]) == 4 and 1920 <= int(p["byr"]) <= 2002
+        val_iyr = len(p["iyr"]) == 4 and 2010 <= int(p["iyr"]) <= 2020
+        val_eyr = len(p["eyr"]) == 4 and 2020 <= int(p["eyr"]) <= 2030
+        val_hgt = (p["hgt"].endswith("in") and  59 <= int(p['hgt'][:-2]) <=  76) or \
+                  (p["hgt"].endswith("cm") and 150 <= int(p['hgt'][:-2]) <= 193)
+        val_hcl = re.fullmatch(r"#[0-9a-f]{6}", p["hcl"])
+        val_ecl = p["ecl"] in ["amb", "blu", "brn", "gry" ,"grn", "hzl", "oth"]
+        val_pid = re.fullmatch(r"[0-9]{9}", p["pid"])
 
-    return len(to_find) == 0 or (len(to_find) == 1 and to_find[0] == "cid")
+        return all([val_byr, val_iyr, val_eyr, val_hgt, val_hcl, val_ecl, val_pid])
+    except:
+        return False
 
-def valid_and_fields(fields):
-    to_find = ["byr", "iyr", "eyr", "hgt", 'hcl', "ecl", "pid", "cid"]
-    for key in fields.keys():
-        to_find.remove(key)
-
-    if len(to_find) == 0 or (len(to_find) == 1 and to_find[0] == "cid"):
-        for key in fields:
-            val = fields[key]
-            if key == "byr" and not ((len(val) == 4) and (1920 <= int(val) <= 2002)):
-                return False
-            if key == "iyr" and not ((len(val) == 4) and (2010 <= int(val) <= 2020)):
-                return False
-            if key == "eyr" and not ((len(val) == 4) and (2020 <= int(val) <= 2030)):
-                return False
-            if key == "hgt":
-                if val[-2:] == "in" and (59 <= int(val[:-2]) <= 76):
-                    pass
-                elif val[-2:] == "cm" and (150 <= int(val[:-2]) <= 193):
-                    pass
-                else:
-                    return False
-            if key == "hcl" and not re.fullmatch(r"#[0-9a-f]{6}", val):
-                return False
-            if key == "ecl" and val not in ["amb", "blu", "brn", "gry" ,"grn", "hzl", "oth"]:
-                return False
-            if key == "pid" and not re.fullmatch(r"[0-9]{9}", val):
-                return False
-
-        return True
-    else:
-        return False        
-                
-
+def has_fields(passport: dict) -> bool:
+    sym_diff = set(passport.keys()).symmetric_difference({"byr", "iyr", "eyr", "hgt", 'hcl', "ecl", "pid", "cid"})
+    return sym_diff <= {"cid"}
 
 def run() -> (int, int):
+
     with open("input/4.txt") as file:
-        lines = [line.strip() for line in file]
+        lines = [line.replace('\n', ' ') for line in file.read().split("\n\n")]
+        passports = [ dict(val.split(":") for val in line.split()) 
+                     for line in lines ]
+        all_fields = [p for p in passports if has_fields(p)]
+        valid_fields = [p for p in passports if has_valid_fields(p)] 
 
-        fields = {}
-        count_p1 = 0
-        count_p2 = 0
-
-        for line in lines:
-            # print(line)
-            if len(line) == 0:
-                
-                # print(fields)
-                to_find = ["byr", "iyr", "eyr", "hgt", 'hcl', "ecl", "pid", "cid"]
-                for key in fields.keys():
-                    to_find.remove(key)
-
-                count_p1 += valid(fields)
-                count_p2 += valid_and_fields(fields)
-                fields = {}
-            else:
-                entries = line.split(' ')
-                for entry in entries:
-                    # print(f"\t{entry}")
-                    [key, val] = entry.split(':')
-                    fields[key] = val
-
-        to_find = ["byr", "iyr", "eyr", "hgt", 'hcl', "ecl", "pid", "cid"]
-        for key in fields.keys():
-            to_find.remove(key)
-
-        count_p1 += valid(fields)
-        count_p2 += valid_and_fields(fields)
-
-        return (count_p1, count_p2)
+        return len(all_fields), len(valid_fields)
 
 if __name__ == "__main__":
     print(run())
