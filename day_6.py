0297b7d6543dc868d7c8489bf2e3caa7afb40047
ajmorton <ajmorton2@gmail.com>
2020-12-06 17:05:37 +1100

    Day 6 - opt 2

--- day_6.py
+++ day_6.py
@@ -3,11 +3,14 @@ from functools import reduce
 def run() -> (int, int):
 
     with open("input/6.txt") as file:
-        groups = [line.split('\n') for line in file.read().rstrip('\n').split("\n\n")]
-        answer_sets = [[set(answer) for answer in group] for group in groups]
+        groups = [line.rstrip('\n').split('\n') for line in file.read().split("\n\n")]
+        answer_sets = [list(map(set, group)) for group in groups]
 
-        sum_any = sum([len(reduce(lambda x, y: x.union(y), answer_set)) for answer_set in answer_sets])
-        sum_all = sum([len(reduce(lambda x, y: x.intersection(y), answer_set)) for answer_set in answer_sets])
+        sum_any, sum_all = 0, 0
+        for answer_set in answer_sets:
+            sum_any += len(reduce(set.union, answer_set))
+            sum_all += len(reduce(set.intersection, answer_set))
+
         return(sum_any, sum_all)
 
 if __name__ == "__main__":
