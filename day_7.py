0525cb1bb178a582e5fab657b3e48707f28b02bb
ajmorton <ajmorton2@gmail.com>
2020-12-08 18:45:00 +1100

    Day 7 - opt

--- day_7.py
+++ day_7.py
@@ -1,45 +1,22 @@
 import read_as
+import re
+from collections import defaultdict
 
-def contains_shiny_gold(bag: str, bag_map: dict, memo: dict) -> int:
-
-    result = False
-
-    if bag[1] in memo:
-        return memo[bag[1]]
-    elif bag == "shiny gold":
-        result = True
-    else:
-        result = any([contains_shiny_gold(sub_bag[1], bag_map, memo) for sub_bag in bag_map[bag]])
-
-    memo[bag] = result
-    return result
-
-def contains(bag: str, bag_map: dict) -> int:
-    return 1 + sum([num * contains(bag, bag_map) for (num, bag) in bag_map[bag]])
-
+def contains_shiny_gold(bag: str, bag_map: dict) -> int:
+    # faster without memo ¯\_(ツ)_/¯
+    return bag == "shiny gold" or any([contains_shiny_gold(sub_bag[1], bag_map) for sub_bag in bag_map[bag]])
     
+def contains(bag: str, bag_map: dict) -> int:
+    return 1 + sum([int(num) * contains(bag, bag_map) for (num, bag) in bag_map[bag]])
+
 def run() -> (int, int):
-    lines = read_as.lines("input/7.txt")
-    bag_map = {}
-    for line in lines:
+    bag_map = defaultdict(list)
+    for line in read_as.lines("input/7.txt"):
         (container, contents) = line.split(" bags contain ")
+        contents = re.findall(r"([0-9]+) ([a-z ]+) bags?[,.]", contents)
+        bag_map[container].extend(contents)
 
-        if contents == "no other bags.":
-            bag_map[container] = []
-        else:
-            # "fuschia bags".rstrip(" bags") == "fuschi", not "fuschia" !!!
-            contents = [bag.strip().rstrip("bags").rstrip() for bag in contents.rstrip('.').split(',')]
-            for bag in contents:
-                (num, bag) = bag.split(' ', 1)
-                num = int(num)
-                if container in bag_map:
-                    bag_map[container].append((num, bag))
-                else:
-                    bag_map[container] = [(num, bag)]
-
-    memo = {}
-    bags_containing = [bag for bag in bag_map if bag != "shiny gold" and contains_shiny_gold(bag, bag_map, {})]
-
+    bags_containing = [bag for bag in bag_map if bag != "shiny gold" and contains_shiny_gold(bag, bag_map)]
     return(len(bags_containing), contains("shiny gold", bag_map) - 1)
 
 if __name__ == "__main__":
