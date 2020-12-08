d4758d14b76f84d61de6fd85a7a8268128bafd55
ajmorton <ajmorton2@gmail.com>
2020-12-08 18:00:50 +1100

    Day 8 - opt

--- day_8.py
+++ day_8.py
@@ -1,95 +1,50 @@
 import read_as
 
-def execute(cur_instr, ptr, instrs, acc, visited) -> (True, int):
-    command, val = cur_instr
-    
-    try:
-        if ptr > len(instrs):
+def execute(prog):
+    ptr, acc = 0, 0
+    visited = set()
+
+    while True:
+        if ptr == len(prog):
             return True, acc
-        if visited[ptr] == True:
+        elif ptr < 0 or ptr > len(prog) or ptr in visited:
             return False, acc
         else:
-            visited[ptr] = True
-            if command == "nop":
-                if ptr == len(instrs) - 1:
-                    return True, acc
-                return execute(instrs[ptr + 1], ptr + 1, instrs, acc, visited)
-            elif command == "acc":
-                if ptr == len(instrs) - 1:
-                    return True, acc + val
-                return execute(instrs[ptr + 1], ptr + 1, instrs, acc + val, visited)
-            elif command == "jmp":
-                if ptr + val < 0 or ptr + val >= len(instrs):
-                    return False, acc
-                return execute(instrs[ptr + val], ptr + val, instrs, acc, visited)
-    except:
-        return False, 0
-
-
-def run_to_end(instrs):
-    ptr = 0
-    acc = 0
-    visited = [False] * len(instrs)
-
-    instr_count = 0
-
-    while instr_count < 100000:
-        instr_count += 1
-        if ptr < 0 or ptr > len(instrs):
-            return False, -1
-        elif ptr == len(instrs):
-            return True, acc
-        else:
-            cur_instr, val = instrs[ptr]
+            cur_instr, val = prog[ptr]
+            visited.add(ptr)
             if cur_instr == "nop":
                 ptr += 1
             elif cur_instr == "jmp":
                 ptr += val
             elif cur_instr == "acc":
-                acc +=  val
+                acc += val
                 ptr += 1
             else:
                 print("ERROR")
 
-    return False, "-2"
-
-
-
 
 def run() -> (int, int):
-    lines = read_as.lines("input/8.txt")
 
-    instrs = []
-    for line in lines:
-        instr, val = line.split(' ')
-        val = int(val)
-        instrs.append((instr, val))
+    to_instr = lambda x: (x[0], int(x[1]))
+    prog = [to_instr(line.split(' ')) for line in read_as.lines("input/8.txt")]
 
+    _, p1 = execute(prog)
 
-    acc = 0
-    ptr = 0
+    for i in range(0, len(prog)):
+        new_instr = None
+        if prog[i][0] == "jmp":
+            new_instr = ("nop", prog[i][1])
+        elif prog[i][0] == "nop":
+            new_instr = ("jmp", prog[i][1])
 
-    visited = [False] * len(instrs)
+        if new_instr:
+            new_prog = prog.copy()
+            new_prog[i] = new_instr
 
-    p1 = execute(instrs[0], ptr, instrs, acc, visited)[1]
-
-    for i in range(0, len(instrs)):
-        if instrs[i][0] == "jmp":
-            new_instrs = instrs.copy()
-            new_instrs[i] = ("nop", instrs[i][1])
-            result = run_to_end(new_instrs)
+            result = execute(new_prog)
             if result[0] == True:
                 p2 = result[1]
                 break
-
-        elif instrs[i][0] == "nop":
-            new_instrs = instrs.copy()
-            new_instrs[i] = ("jmp", instrs[i][1])
-            result = run_to_end(new_instrs)
-            if result[0] == True:
-                p2 = result[1]
-                break
-
 
     return(p1, p2)
 
