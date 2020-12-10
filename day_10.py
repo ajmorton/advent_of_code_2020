f338de8c6ce10588e8398be5ae5a02dc5ad43220
ajmorton <ajmorton2@gmail.com>
2020-12-10 18:49:33 +1100

    Day 10 - opt

--- day_10.py
+++ day_10.py
@@ -1,38 +1,21 @@
 import read_as
-from collections import defaultdict
-import itertools
+from collections import Counter, defaultdict
 
-def paths_to_target(adaptors, cur_volt, target, memo) -> bool:
-
-    state = (len(adaptors), cur_volt)
-    if state in memo:
-        return memo[state]
-
-    paths = 0
-    if abs(cur_volt - target) <= 3:
-        paths += 1
-    if len(adaptors) == 0:
-        return paths
-    if abs(adaptors[0] - cur_volt) <= 3:
-        paths += paths_to_target(adaptors[1:], adaptors[0], target, memo)
-    paths += paths_to_target(adaptors[1:], cur_volt, target, memo)
-
-    memo[state] = paths
-    return paths
+def paths_to_target(adapters) -> int:
+    paths_to = defaultdict(int)
+    paths_to[0] = 1
+    for adapter in adapters:
+        paths_to[adapter] = paths_to[adapter - 1] + paths_to[adapter - 2] + paths_to[adapter - 3]
+    return paths_to[adapters[-1]]
 
 def run() -> (int, int):
+    adapters = sorted([int(line) for line in read_as.lines("input/10.txt")])
 
-    adaptors = [int(line) for line in read_as.lines("input/10.txt")]
-    adaptors.sort()
-
-    diffs = defaultdict(int)
-    for i in range(1, len(adaptors)):
-        diffs[adaptors[i] - adaptors[i-1]] += 1
-
-    diffs[adaptors[0]] += 1
-    diffs[3] += 1
+    jumps = Counter([adapters[i] - adapters[i-1] for i in range(1, len(adapters))])
+    jumps[adapters[0]] += 1
+    jumps[3] += 1
     
-    return(diffs[1] * diffs[3], paths_to_target(adaptors, 0, adaptors[-1] + 3, {}))
+    return(jumps[1] * jumps[3], paths_to_target(adapters))
 
 if __name__ == "__main__":
     print(run())
