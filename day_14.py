d4296d35df91a5586f45588143bdc3abd014ba9e
ajmorton <ajmorton2@gmail.com>
2020-12-14 20:07:55 +1100

    Day 14 - opt

--- day_14.py
+++ day_14.py
@@ -4,58 +4,41 @@ import re
 def all_perms(ls):
     if len(ls) == 0:
         return [[]]
-    if ls[0] == '1':
-        return [['1'] + sub for sub in all_perms(ls[1:])]
-    elif ls[0] == '0':
-        return [['0'] + sub for sub in all_perms(ls[1:])]
+    elif ls[0] in {'0', '1'}:
+        return [[ls[0]] + sub for sub in all_perms(ls[1:])]
     else:
-        subs = [['0'] + sub for sub in all_perms(ls[1:])]
-        subs.extend([['1'] + sub for sub in all_perms(ls[1:])])
-        return subs
+        return [['0'] + sub for sub in all_perms(ls[1:])] + [['1'] + sub for sub in all_perms(ls[1:])]
         
 def run() -> (int, int):
     lines = read_as.lines("input/14.txt")
 
-    vals_p1 = {}
-    vals_p2 = {}
+    results_p1, results_p2 = {}, {}
+    mask_bits_p1, mask_bits_p2 = [], []
 
-    mask_vals_p1 = []
-    mask_vals_p2 = []
     for line in lines:
-        if line[0:7] == "mask = ":
-            mask_vals_p1 = []
-            mask_vals_p2 = []
-            new_mask = line[7:]
-            for i in range(len(new_mask)):
-                if new_mask[i] != "X":
-                    mask_vals_p1.append((i, new_mask[i]))
-                if new_mask[i] != "0":
-                    mask_vals_p2.append((i, new_mask[i]))
+        op, arg = line.split(" = ")
+        if op == "mask":
+            mask_bits_p1 = [(i, char) for (i, char) in enumerate(arg) if char != "X"]
+            mask_bits_p2 = [(i, char) for (i, char) in enumerate(arg) if char != "0"]
 
         else:
-            assign, val = line.split("=")
+            index = int(op[4:-1])
 
             # P1
-            index_p1 = int(re.search("mem\[([0-9]+)\]", assign).groups()[0])
-            bitstring = list("{0:b}".format(int(val)).zfill(36))
-            for (i, m) in mask_vals_p1:
+            bitstring = list("{0:036b}".format(int(arg)))
+            for (i, m) in mask_bits_p1:
                 bitstring[i] = m
-
-            vals_p1[index_p1] = int("".join(bitstring), 2)
+            results_p1[index] = int("".join(bitstring), 2)
 
             # P2
-            val = int(val)
-            index = int(re.search("mem\[([0-9]+)\]", assign).groups()[0])
-            indexstring = list("{0:b}".format(index).zfill(36))
-            for (i, m) in mask_vals_p2:
+            indexstring = list("{0:036b}".format(index))
+            for (i, m) in mask_bits_p2:
                 indexstring[i] = m
 
-            adresses = all_perms(indexstring)
+            for addr in all_perms(indexstring):
+                results_p2[int("".join(addr), 2)] = int(arg)
 
-            for addr in adresses:
-                vals_p2[int("".join(addr), 2)] = val
-
-    return (sum(vals_p1.values()), sum(vals_p2.values()))
+    return (sum(results_p1.values()), sum(results_p2.values()))
 
 if __name__ == "__main__":
     print(run())
