806cac9aaaa3f6b9b7a74f5b5da6d9db6c5f59d2
ajmorton <ajmorton2@gmail.com>
2020-12-15 19:39:36 +1100

    Day 15 - opt

--- day_15.py
+++ day_15.py
@@ -1,39 +1,14 @@
-import read_as
-import re
-
 def run() -> (int, int):
-    lines = read_as.lines("input/15.txt")
-
     nums = [0,8,15,2,12,1]
-    nums_map = {v: i +1 for (i, v) in enumerate(nums)}
+    nums_map = {v: i + 1 for (i, v) in enumerate(nums)}
 
-    cur_index = len(nums) + 1
     next_val = 4
+    p1 = 0
 
-    while cur_index < 2020:
-        if next_val not in nums_map:
-            new_val = 0
-        else:
-            new_val = cur_index - nums_map[next_val]
-
-        nums_map[next_val] = cur_index
-
-        cur_index += 1
-        next_val = new_val
-
-    p1 = next_val
-
-    while cur_index < 30000000:
-        if next_val not in nums_map:
-            new_val = 0
-        else:
-            new_val = cur_index - nums_map[next_val]
-
-        nums_map[next_val] = cur_index
-
-        cur_index += 1
-        next_val = new_val
-
+    for i in range(len(nums) + 1, 30000000):
+        nums_map[next_val], next_val = i, i - nums_map[next_val] if next_val in nums_map else 0
+        if i == 2019:
+            p1 = next_val
     p2 = next_val
 
     return (p1, p2)
