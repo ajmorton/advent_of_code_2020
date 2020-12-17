7e0f47de0f013df92bb872f68f443b03d19ebe8b
ajmorton <ajmorton2@gmail.com>
2020-12-17 19:44:55 +1100

    Day 17 - opt

--- day_17.py
+++ day_17.py
@@ -1,59 +1,42 @@
 import read_as
-import re
-from math import inf
 from collections import defaultdict
 from copy import deepcopy
+import itertools
 
 def run() -> (int, int):
     slice = read_as.grid("input/17.txt")
 
-    state = defaultdict(int)
+    state = defaultdict(lambda: (False,False))
 
     for r in range(len(slice)):
         for c in range(len(slice[0])):
-            state[(0, r, c, 0)] = slice[r][c] == "#" 
+            state[(0, r, c, 0)] = (slice[r][c] == "#", slice[r][c] == "#")
 
-    min_z, max_z = 0, 0
-    min_r, max_r = 0, len(slice)
-    min_c, max_c = 0, len(slice[0])
-    min_d, max_d = 0, 0
+    init_r, init_c = len(slice), len(slice[0])
 
     cycle = 1
     while cycle <= 6:
+        next_state = defaultdict(lambda: (False,False))
 
-        next_state = defaultdict(int)
+        for z, r, c, d in itertools.product(range(-cycle, cycle +1), range(-cycle, init_r + cycle + 1), range(-cycle, init_c + cycle + 1), range(-cycle, cycle+1)):
+            neighbours_3d, neighbours_4d = 0, 0
+            for m,n,o,p in itertools.product([-1, 0, 1], repeat=4):
+                if not (m == n == o == p == 0):
+                    neighbours_4d += state[(z+m, r+n, c+o, d+p)][1]
+                    if d == p == 0:
+                        neighbours_3d += state[(z+m, r+n, c+o, d+p)][0]
 
-        for z in range(min_z - 1, max_z + 2):
-            for r in range(min_r -1, max_r + 2):
-                for c in range(min_c -1,  max_c + 2):
-                    for d in range(min_d -1, max_d + 2):
-                        neigbours = 0
-                        for m in [-1, 0, 1]:
-                            for n in [-1, 0, 1]:
-                                for o in [-1, 0, 1]:
-                                    for p in [-1, 0, 1]:
-                                        if m == n == o == p == 0:
-                                            continue
-                                        else:
-                                            neigbours += state[(z+m,r+n,c+o,d+p)]
-                        if state[(z,r,c,d)] == True and neigbours not in {2,3}:
-                            next_state[(z,r,c,d)] = False
-                        elif state[(z,r,c,d)] == False and neigbours == 3:
-                            next_state[(z,r,c,d)] = True
-                        else:
-                            next_state[(z,r,c,d)] = state[(z,r,c,d)]
+            cube_3d_active = (state[(z,r,c,d)][0] and neighbours_3d in {2,3}) or (not state[(z,r,c,d)][0] and neighbours_3d == 3)
+            cube_4d_active = (state[(z,r,c,d)][1] and neighbours_4d in {2,3}) or (not state[(z,r,c,d)][1] and neighbours_4d == 3)
+            next_state[(z,r,c,d)] = (cube_3d_active, cube_4d_active)
 
         state = next_state
-        min_z, max_z = min_z - 1, max_z + 1
-        min_r, max_r = min_r - 1, max_r + 1
-        min_c, max_c = min_c - 1, max_c + 1
-        min_d, max_d = min_d - 1, max_d + 1
         cycle += 1
 
-    p1 = 336
-    total_cubes = sum(state.values())
+    total_3d_cubes = sum( [state[coord][0] for coord in state] )
+    total_4d_cubes = sum( [state[coord][1] for coord in state] )
 
-    return (p1, total_cubes)
+    return (total_3d_cubes, total_4d_cubes)
 
 if __name__ == "__main__":
     print(run())
