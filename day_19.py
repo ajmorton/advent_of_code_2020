af7ae907c0e798fdbb22707d8adecd2cd15b6557
ajmorton <ajmorton2@gmail.com>
2020-12-19 17:46:02 +1100

    Day 19 - opt

--- day_19.py
+++ day_19.py
@@ -2,6 +2,18 @@ import read_as
 import re
 from copy import deepcopy
 
+def rules_to_regex(rules: dict) -> dict:
+    no_nums = False
+    while no_nums == False:
+        no_nums = True
+        for (rule_id, rule_contents) in rules.items():
+            if re.search(r"[0-9]+", rule_contents):
+                no_nums = False
+            else:
+                for other_rule in rules:
+                    rules[other_rule] = re.sub(" " + rule_id + " ", " " + rule_contents + " ", rules[other_rule])
+    return rules
+
 def run() -> (int, int):
     groups = read_as.groups("input/19.txt")
     rules = {}
@@ -15,36 +27,16 @@ def run() -> (int, int):
         else:
             rules[rule_id] = "( " + rule_contents.strip() + " )"
 
+
+    rules_p1 = rules_to_regex(deepcopy(rules))
+
     rules_p2 = deepcopy(rules)        
-
-    no_nums = False
-    while no_nums == False:
-        no_nums = True
-        for (rule_id, rule_contents) in rules.items():
-            if re.search(r"[0-9]+", rule_contents):
-                no_nums = False
-            else:
-                for other_rule in rules:
-                    rules[other_rule] = re.sub(" " + rule_id + " ", " " + rule_contents + " ", rules[other_rule])
-
-
-    rules_p2["8"] = "( 42 | ( 42 )+ )"
+    rules_p2["8"] = "( 42 )+"
     # dirty hack
     rules_p2["11"] = "( 42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31 | 42 42 42 42 42 42 31 31 31 31 31 31 | 42 42 42 42 42 42 42 31 31 31 31 31 31 31 | 42 42 42 42 42 42 42 42 31 31 31 31 31 31 31 31 | 42 42 42 42 42 42 42 42 42 31 31 31 31 31 31 31 31 31 | 42 42 42 42 42 42 42 42 42 42 31 31 31 31 31 31 31 31 31 31 )"
+    rules_p2 = rules_to_regex(rules_p2)
 
-    no_nums = False
-    while no_nums == False:
-        no_nums = True
-        for (rule_id, rule_contents) in rules_p2.items():
-            if re.search(r"[0-9]+", rule_contents):
-                no_nums = False
-            else:
-                for other_rule in rules_p2:
-                    rules_p2[other_rule] = re.sub(" " + rule_id + " ", " " + rule_contents + " ", rules_p2[other_rule])
-
-
-
-    p1 = sum([re.fullmatch(rules["0"].replace(" ", ''), message) is not None for message in groups[1]])
+    p1 = sum([re.fullmatch(rules_p1["0"].replace(" ", ''), message) is not None for message in groups[1]])
     p2 = sum([re.fullmatch(rules_p2["0"].replace(" ", ''), message) is not None for message in groups[1]])
 
     return (p1, p2)
