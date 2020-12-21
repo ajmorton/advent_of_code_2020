917fea24668ae0e829bf0fe834e2bd753d2beef5
ajmorton <ajmorton2@gmail.com>
2020-12-21 17:14:42 +1100

    Day 21 - opt

--- day_21.py
+++ day_21.py
@@ -7,57 +7,30 @@ def run() -> (int, int):
     lines = read_as.lines("input/21.txt")
 
     possible = defaultdict(list)
-
     all_ingreds = []
 
     for line in lines:
-        ingredients = None
-        allergens = None
-        if "(" in line:
-            ingredients, allergens = line.split("(contains")
-            ingredients = ingredients.strip().split(' ')
-            if allergens:
-                allergens = allergens.strip(' )').split(", ")
-        else:
-            ingredients = line.strip().split(' ')            
+        ingredients, allergens = line.split("(contains")
+
+        ingredients = ingredients.strip().split(' ')
+        allergens = allergens.strip(' )').split(", ")
 
         all_ingreds.extend(ingredients)
 
-        ingredients = set(ingredients)
-        allergens = set(allergens)
-
         for allergen in allergens:
-            possible[allergen].append(ingredients)
+            possible[allergen].append(set(ingredients))
 
+    possible = {a: set.intersection(*i) for (a,i) in possible.items()}
+    
+    known_allergens = {}
+    while len(possible) > 0:
+        known = [(i, deepcopy(a).pop()) for (i, a) in possible.items() if len(a) == 1]
+        for (known_a, known_i) in known:
+            known_allergens[known_a] = known_i
+            possible = {a: {x for x in i if x != known_i} for (a, i) in possible.items() if a != known_a}
 
-    reduced = {}
-    for p in possible:
-        can_be = set.intersection(*possible[p])
-        reduced[p] = can_be
-
-    answers = {}
-    while len(reduced) > 0:
-        choice, aller_choice = None, None
-        for (ingred, aller) in reduced.items():
-            if len(aller) == 1:
-                choice, aller_choice = ingred, aller
-                break
-        
-        if choice is not None:
-            answers[choice] = aller_choice
-            for r, r_aller in reduced.items():
-                reduced[r] = {a for a in r_aller if a not in aller_choice}
-            reduced.pop(choice)
-        else:
-            print("F")
-
-    ingreds = set.union(*answers.values())
-
-    total_non_allergens = len([i for i in all_ingreds if i not in ingreds])
-
-    answers = {a: i.pop() for (a,i) in answers.items()}
-    canonical_danger_list = [a for (i, a) in sorted(answers.items(), key=lambda x: x[0])]
-    print(canonical_danger_list)
+    total_non_allergens = len([i for i in all_ingreds if i not in known_allergens.values()])
+    canonical_danger_list = [i for (a, i) in sorted(known_allergens.items(), key=lambda x: x[0])]
 
     return (total_non_allergens, ",".join(canonical_danger_list))
 
