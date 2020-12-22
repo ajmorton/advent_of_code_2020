cb4efc002f7c765ce737aff182aa9d0c05143e69
ajmorton <ajmorton2@gmail.com>
2020-12-22 17:50:14 +1100

    Day 22 - opt

--- day_22.py
+++ day_22.py
@@ -1,77 +1,40 @@
 import read_as
-import re
-from copy import deepcopy
-from collections import defaultdict
 
-def recursive_combat(player_cards, crab_cards, history):
-
+def recursive_combat(player_cards, crab_cards, history, part_2):
     while len(player_cards) > 0 and len(crab_cards) > 0:
         if (tuple(player_cards), tuple(crab_cards)) in history:
-            return True, player_cards # player wins
+            return True, player_cards
         else:
             history[(tuple(player_cards), tuple(crab_cards))] = 1
 
-        # draw
-        player, player_cards = player_cards[0], player_cards[1:]
-        crab, crab_cards = crab_cards[0], crab_cards[1:]
+        player_card, player_cards = player_cards[0], player_cards[1:]
+        crab_card, crab_cards = crab_cards[0], crab_cards[1:]
 
-        if player <= len(player_cards) and crab <= len(crab_cards):
-            winner, _ = recursive_combat(player_cards[:player], crab_cards[:crab], {})
-            if winner:
-                player_cards.extend([player, crab])
+        if part_2 and player_card <= len(player_cards) and crab_card <= len(crab_cards):
+            player_wins, _ = recursive_combat(player_cards[:player_card], crab_cards[:crab_card], {}, part_2)
+            if player_wins:
+                player_cards.extend([player_card, crab_card])
             else:
-                crab_cards.extend([crab, player])
+                crab_cards.extend([crab_card, player_card])
         else:
-            if player > crab:
-                player_cards.extend([player, crab])
+            if player_card > crab_card:
+                player_cards.extend([player_card, crab_card])
             else:
-                crab_cards.extend([crab, player])
+                crab_cards.extend([crab_card, player_card])
 
-    if len(player_cards) > 0:
-        return True, player_cards
-    else:
-        return False, crab_cards
-
-def combat(player_cards, crab_cards):
-    if len(crab_cards) == 0:
-        return player_cards
-    elif len(player_cards) == 0:
-        return crab_cards
-    
-    player, player_cards = player_cards[0], player_cards[1:]
-    crab, crab_cards = crab_cards[0], crab_cards[1:]
-
-    if player > crab:
-        player_cards.extend([player, crab])
-    else:
-        crab_cards.extend([crab, player])
-
-    return combat(player_cards, crab_cards)
-
+    return (True, player_cards) if len(player_cards) > 0 else (False, crab_cards)
 
 def run() -> (int, int):
     groups = read_as.groups("input/22.txt")
     player_cards = [int(n) for n in groups[0][1:]]
     crab_cards = [int(n) for n in groups[1][1:]]
 
-    prev_hands = {}
+    _, winner_hand_p1 = recursive_combat(player_cards, crab_cards, {}, False)
+    _, winner_hand_p2 = recursive_combat(player_cards, crab_cards, {}, True)
 
-    winner_hand = combat(player_cards, crab_cards)
-    _, hand_p2 = recursive_combat(player_cards, crab_cards, {})
-
-    score = 0
-    mult = 1
-    for card in winner_hand[::-1]:
-        score += mult * card
-        mult += 1
-
-    score_p2 = 0
-    mult = 1
-    for card in hand_p2[::-1]:
-        score_p2 += mult * card
-        mult += 1
-
-    return (score, score_p2)
+    score_p1 = sum([(i+1) * n for (i, n) in enumerate(winner_hand_p1[::-1])])
+    score_p2 = sum([(i+1) * n for (i, n) in enumerate(winner_hand_p2[::-1])])
+    return (score_p1, score_p2)
 
 if __name__ == "__main__":
     print(run())
