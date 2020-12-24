503fae7b398776998b93a230536e88cd94ab02e5
ajmorton <ajmorton2@gmail.com>
2020-12-24 17:51:28 +1100

    Day 24 - opt

--- day_24.py
+++ day_24.py
@@ -1,70 +1,38 @@
 import read_as
-import itertools as it
-from collections import defaultdict
 import re
+from collections import Counter
 
-def update(orig_tiles):
-    black_neighbours = defaultdict(int)
+def update(tiles, n):
+    for _ in range(n):
+        neighbours_black = Counter()
 
-    for (ns, ew) in orig_tiles:
-        black_neighbours[(ns, ew - 2)] += 1
-        black_neighbours[(ns, ew + 2)] += 1
-        black_neighbours[(ns + 1, ew - 1)] += 1
-        black_neighbours[(ns - 1, ew - 1)] += 1
-        black_neighbours[(ns + 1, ew + 1)] += 1
-        black_neighbours[(ns - 1, ew + 1)] += 1
+        for (n, e) in tiles:
+            neighbours_black.update({(n, e+2), (n+1, e+1), (n-1, e+1), (n, e-2), (n+1, e-1), (n-1, e-1)})
 
-    next_day = set()
-    for tile in black_neighbours:
-        if tile in orig_tiles and black_neighbours[tile] in {1,2}:
-            next_day.add(tile)
-        elif tile not in orig_tiles and black_neighbours[tile] == 2:
-            next_day.add(tile)
+        flip_black = set(tile for tile in neighbours_black if tile not in tiles and neighbours_black[tile] == 2)
+        stay_black = set(tile for tile in neighbours_black if tile in tiles and neighbours_black[tile] in {1,2})
+        tiles = flip_black.union(stay_black)
 
-    return next_day
+    return tiles
 
 def run() -> (int, int):
-    lines = read_as.lines("input/24.txt")
-
     visited = set()
 
-    next_dir = ""
-    for path in lines:
-        cur_ew, cur_ns = 0, 0
-        for char in path:
-            next_dir += char
-            if next_dir == 'e':
-                cur_ew += 2
-            elif next_dir == 'se':
-                cur_ew += 1
-                cur_ns -= 1
-            elif next_dir == 'sw':
-                cur_ew -= 1
-                cur_ns -= 1
-            elif next_dir == 'w':
-                cur_ew -= 2
-            elif next_dir == 'nw':
-                cur_ew -= 1
-                cur_ns += 1
-            elif next_dir == 'ne':
-                cur_ew += 1
-                cur_ns += 1
-            else:
-                continue
+    for path in read_as.lines("input/24.txt"):
+        north, east = 0, 0
+        for move in re.findall("[ns]?[ew]", path):
+            north, east = {
+                'e':  (north,     east + 2),
+                'ne': (north + 1, east + 1),
+                'se': (north - 1, east + 1),
+                'w':  (north,     east - 2),
+                'nw': (north + 1, east - 1),
+                'sw': (north - 1, east - 1)
+            }[move]
 
-        
-            next_dir = ""
-        if (cur_ns, cur_ew) in visited:
-            visited.remove((cur_ns, cur_ew))
-        else:
-            visited.add( (cur_ns, cur_ew) )
+        visited ^= {(north, east)}
 
-    p1 = len(visited)
-
-    for i in range(100):
-        visited = update(visited)
-
-    return (p1, len(visited))
+    return (len(visited), len(update(visited, 100)))
 
 if __name__ == "__main__":
     print(run())
