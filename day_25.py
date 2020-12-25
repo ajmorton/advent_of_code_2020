33d3fc21811fca0f3f088664b008c40bf5e7f0d0
ajmorton <ajmorton2@gmail.com>
2020-12-25 16:52:05 +1100

    Day 25 - opt

--- day_25.py
+++ day_25.py
@@ -1,38 +1,15 @@
 import read_as
-import re
-from collections import Counter
 
 def run() -> (int, int):
-    lines = read_as.lines("input/25.txt")
-    pub_1 = int(lines[0])
-    pub_2 = int(lines[1])
+    pub_1, pub_2 = [int(line) for line in read_as.lines("input/25.txt")]
+    m = 20201227
 
-    base = 7
-    
-    i = 1
-    val = 1
-    found = 0
-    encr_1 , encr_2 = 0, 0
-    while found < 2:
-        val *= 7
-        val %= 20201227
+    loop, n = 1, 1
+    while (n := (n*7) % m) not in {pub_1, pub_2}:
+        loop += 1
 
-        if val == pub_1:
-            encr_1 = i
-            found += 1
-        elif val == pub_2:
-            encr_2 = i
-            found += 1
-
-        i+=1
-
-    val_2 = 1
-    for i in range(encr_2):
-        val_2 *= pub_1
-        val_2 %= 20201227
-
-    return (val_2, False)
-
+    pub = pub_1 if n != pub_1 else pub_2
+    return ( pow(pub, loop, mod=m), False)
 
 if __name__ == "__main__":
     print(run())
